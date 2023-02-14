6bd194fe8ea080e2bf2d807b27e305afc01e551a
Ebenezer <58320436+thebenezer@users.noreply.github.com>
2023-02-14 20:29:08 +0530

    added sharing feature

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,37 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import Stats from 'stats.js';
 
+const stats = new Stats();
+document.body.appendChild( stats.dom );
+// if (import.meta.env.PROD) {
+  stats.dom.style.display='none'
+// }
+
+// Get references to the input fields and the share button
+const name1Input = document.getElementById('name1-input');
+const name2Input = document.getElementById('name2-input');
+const colorOptions = document.querySelectorAll('.color-option');
+const colorContainers = document.querySelectorAll('.colorContainer');
+const showColorsButton = document.getElementById('show-colors-button');
+const shareBtn = document.getElementById('share-btn');
+const heart = document.getElementById('heart');
+const inputEntry = document.getElementById('inputEntry');
 
 let controls: OrbitControls;
 let camera: THREE.PerspectiveCamera;
 let renderer: THREE.WebGLRenderer;
 let scene: THREE.Scene;
 let instancedMesh: THREE.InstancedMesh;
+let balloonMaterial: THREE.MeshStandardMaterial;
 let confettiInstance: THREE.InstancedMesh;
 let matrix = new THREE.Matrix4() 
 let mat = new THREE.Matrix4()
 
-const stats = new Stats();
-document.body.appendChild( stats.dom );
+
 
 const listener = new THREE.AudioListener();
 const loader = new THREE.AudioLoader();
-
+let soundReady = false;
 // let soundReady = false;
 const swoosh = new THREE.Audio(listener)
 
@@ -27,7 +42,7 @@ var mouse = new THREE.Vector2();
 const confettiVelocities: any[] = [];
 const confettiRotations = [];
 
-const balloonSpeed: number[] = [];
+const balloonSpeeds: number[] = [];
 
 init();
 requestAnimationFrame(tick);
@@ -35,7 +50,7 @@ requestAnimationFrame(tick);
 function init() {
   // Camera
   camera = new THREE.PerspectiveCamera(
-    45,
+    30,
     window.innerWidth / window.innerHeight,
     1,
     300,
@@ -44,24 +59,26 @@ function init() {
 
   // Scene
   scene = new THREE.Scene();
-  scene.background = new THREE.Color(0xfefefe);
+  // scene.background = new THREE.Color(0xfefefe);
 
   // Renderer
-  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer = new THREE.WebGLRenderer({ antialias: true,alpha:true });
   renderer.shadowMap.enabled = true;
   renderer.shadowMap.autoUpdate = true;
-  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+  // renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  renderer.toneMapping = THREE.ACESFilmicToneMapping;
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
   renderer.render(scene, camera);
   document.body.appendChild(renderer.domElement);
+  renderer.setClearColor( 0x000000, 0 ); // the default
 
   // Fog
-  // scene.fog = new THREE.FogExp2(0xfefefe, 0.05);
+  scene.fog = new THREE.FogExp2(0xfefefe, 0.005);
 
   setupLights();
   setupOrbitControls();
-  setupEventListeners();
   setupSounds();
 
   // ***** setup our scene *******
@@ -69,10 +86,13 @@ function init() {
   createBalloons(400);
   createFetti(500,2.5);
 
+
+  setupEventListeners();
+
   // Pop the balloon on click
   document.addEventListener("click", onDocumentMouseDown, false);
   function onDocumentMouseDown(event: { preventDefault: () => void; clientX: number; clientY: number; }) {
-    event.preventDefault();
+    // event.preventDefault();
     mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1;
     mouse.y = -(event.clientY / renderer.domElement.clientHeight) * 2 + 1;
     var raycaster = new THREE.Raycaster();
@@ -84,7 +104,7 @@ function init() {
       updateFetti(intersects[0].point,2.5);
 
       // Remove the intersected balloon
-      matrix.setPosition(Math.random() * 30 - 10, -10, Math.random() * 30 - 10);
+      matrix.setPosition(Math.random() * 30 - 10, -50, Math.random() * 30 - 10);
       if(!instanceIndex)return;
       instancedMesh.setMatrixAt(instanceIndex, matrix);
 
@@ -123,8 +143,9 @@ function createFetti(count:number,radius:number){
     var rx = Math.random() * 2 * Math.PI;
     var ry = Math.random() * 2 * Math.PI;
     var rz = Math.random() * 2 * Math.PI;
-    mat.makeRotationFromQuaternion(quat.setFromEuler(euler.set(rx, ry, rz, "XYZ")))
-    
+    mat.makeRotationFromQuaternion(quat.setFromEuler(euler.set(rx, ry, rz, "XYZ")));
+    const size = Math.random()*5;
+    mat.scale(new THREE.Vector3(size,size,0))
     var randomDirection = new THREE.Vector3(Math.random()-0.5,Math.random()-0.5,Math.random()-0.5).normalize();
     var randomPosition = randomDirection.multiplyScalar(Math.random()*radius);
     mat.setPosition(randomPosition)
@@ -172,7 +193,7 @@ function tickFetti(){
     if(mat.elements[14]>0) 
       confettiVelocities[i].y += 0.005;
     else confettiVelocities[i].y +=0.001
-    mat.elements[12] += (0.9*confettiVelocities[i].x);
+    mat.elements[12] += (0.9*confettiVelocities[i].x );
     mat.elements[13] += 0.2 - (0.5 * confettiVelocities[i].y);
     // mat.elements[13] += 0.10 - (0.5 * confettiVelocities[i].y);
     mat.elements[14] += (0.9*confettiVelocities[i].z);
@@ -184,21 +205,21 @@ function tickFetti(){
   }
   confettiInstance.instanceMatrix.needsUpdate = true;
 }
+const timer = new THREE.Clock(true)
 // RAF Update the screen
 function tick(): void {
 
   stats.begin();
-
+  const delta = timer.getDelta();
   tickFetti()
-
   for (let i = 0; i < instancedMesh.count; i++) {
     instancedMesh.getMatrixAt(i,matrix);
-    matrix.elements[13] += balloonSpeed[i]*0.05;
+    matrix.elements[13] += balloonSpeeds[i]*3*delta;
     matrix.setPosition(matrix.elements[12], matrix.elements[13], matrix.elements[14]);
 
     // If the balloon is above the screen, position it down again
-    if (matrix.elements[13] > 50) {
-      matrix.setPosition(matrix.elements[12], -50, matrix.elements[14]);
+    if (matrix.elements[13] > 30) {
+      matrix.setPosition(matrix.elements[12], -30, matrix.elements[14]);
 
     }
     instancedMesh.setMatrixAt(i, matrix);
@@ -217,20 +238,20 @@ function createBalloons(count:number){
   var sphereGeometry = new THREE.SphereGeometry( 1, 32, 32 );
 
   // Create a material for the balloons
-  var sphereMaterial = new THREE.MeshStandardMaterial( { 
-    color: 0xff0000,
+  balloonMaterial = new THREE.MeshStandardMaterial( { 
+    color: 0xe91e1e,
     transparent:true,
     opacity:0.5,
     side:THREE.DoubleSide 
   } );
 
   // Create an instanced mesh for the balloons
-  instancedMesh = new THREE.InstancedMesh(sphereGeometry, sphereMaterial, count);
+  instancedMesh = new THREE.InstancedMesh(sphereGeometry, balloonMaterial, count);
   instancedMesh.instanceMatrix.setUsage( THREE.DynamicDrawUsage ); // will be updated every frame
 
   // Set the position and rotation of each instance
   for (var i = 0; i < count; i++) {
-    balloonSpeed.push(Math.random())
+    balloonSpeeds.push(Math.random())
     let matrix = new THREE.Matrix4();
     matrix.setPosition(new THREE.Vector3(
       Math.random() * 100 - 50,
@@ -309,50 +330,178 @@ function setupEventListeners() {
     false,
   );
 
-  const colorOptions = document.querySelectorAll('.color-option');
+
+  
+  // Parse the URL to get the parameter values
+  const urlParams = new URLSearchParams(window.location.search);
+  const name1 = (urlParams.get('a'))
+  const name2 = (urlParams.get('b'))
+  const color = (urlParams.get('c'))
+  // If the URL has valid parameters, update the input fields and balloon color
+  if (name1 && name2 && color) {
+    const text = `To ${decodeURIComponent(name1)}. From ${decodeURIComponent(name2)}`;
+    const textElement = document.createElement('div');
+    textElement.classList.add('text');
+    textElement.innerText = text;
+    document.body.appendChild(textElement);
+    setBalloonColor(decodeURIComponent(color)||'0')
+
+  }
+
+  let balloonColor = '0';
+  // Add a click event listener to the share button
+  (shareBtn as HTMLElement).addEventListener('click', () => {
+    // Get the current values of the input fields and the selected color
+    const name1 = (name1Input as HTMLInputElement).value || "I";
+    const name2 = (name2Input as HTMLInputElement).value || "You";
+    colorOptions.forEach(colorOption => {
+      colorOption.addEventListener('click', () => {
+        balloonColor = (colorOption as HTMLElement).dataset.color||"0";
+      });
+    });
+    const color = balloonColor;
+
+    // Encode the parameter values to be included in the URL
+    const encodedName1 = encodeURIComponent(name1);
+    const encodedName2 = encodeURIComponent(name2);
+    const encodedColor = encodeURIComponent(color);
+
+    // Build the URL string with the parameter values
+    const url = `${window.location.href}?a=${encodedName1}&b=${encodedName2}&c=${encodedColor}`;
+
+
+    // Display the URL in an alert box
+    // alert(`Here's your shareable URL:\n\n${url}`);
+    showPopup(url)
+  });
+
+
+ 
+  (showColorsButton as HTMLElement).addEventListener('click', toggleColorOptions);
+  function toggleColorOptions(){
+    colorContainers.forEach((colorContainer, index) => {
+      console.log((colorContainer as HTMLElement).style.opacity)
+      if (!(colorContainer as HTMLElement).style.opacity || (colorContainer as HTMLElement).style.opacity == "0") {
+        setTimeout(() => {
+          (colorContainer as HTMLElement).style.display = 'block';
+          (colorContainer as HTMLElement).style.opacity = "1";
+          (colorContainer as HTMLElement).style.transform = 'scale(1)';
+        }, index * 100);
+      } else {
+        setTimeout(() => {
+          (colorContainer as HTMLElement).style.opacity = "0";
+          (colorContainer as HTMLElement).style.transform = 'scale(0)';
+          setTimeout(() => {
+            (colorContainer as HTMLElement).style.display = 'none';
+          }, 200);
+        }, (colorContainers.length - index - 1) * 100);
+      }
+    });
+  }
+
   colorOptions.forEach(colorOption => {
     colorOption.addEventListener('click', () => {
       const selectedColor = (colorOption as HTMLElement).dataset.color;
-      if(selectedColor == "red")
-        (instancedMesh.material as THREE.MeshBasicMaterial).color.set(new THREE.Color(0xe91e63))
-      else if(selectedColor == "blue")
-        (instancedMesh.material as THREE.MeshBasicMaterial).color.set(new THREE.Color(0xafeeee))
-      else if(selectedColor == "green")
-        (instancedMesh.material as THREE.MeshBasicMaterial).color.set(new THREE.Color(0x90ee90))
-      else if(selectedColor == "yellow")
-        (instancedMesh.material as THREE.MeshBasicMaterial).color.set(new THREE.Color(0xf0e68c))
-      else if(selectedColor == "pink")
-        (instancedMesh.material as THREE.MeshBasicMaterial).color.set(new THREE.Color(0xffc0cb))
-      else if(selectedColor == "white")
-        (instancedMesh.material as THREE.MeshBasicMaterial).color.set(new THREE.Color(0xffffff))
+      setBalloonColor(selectedColor||'0')
       // Do something with the selected color, such as pass it to your Three.js function.
     });
   });
 
 
-  // const toggleSoundDom = document.getElementById('toggleSound');
-  // // @ts-ignore
-  // toggleSoundDom.addEventListener('click',()=>{toggleSound()},false)
+  const toggleSoundDom = document.getElementById('toggleSound');
+  // @ts-ignore
+  toggleSoundDom.addEventListener('click',()=>{toggleSound()},false)
 
-  // const toggleAnimationDom = document.getElementById('toggleAnimation');
-  // // @ts-ignore
-  // toggleAnimationDom.addEventListener('click',()=>{toggleAnimation()},false)
+  var copyButton = document.getElementById("copy-btn");
+  (copyButton as HTMLElement).addEventListener('click',copyUrl,false);
+  var closePopupButton = document.getElementById("close-popup");
+  (closePopupButton as HTMLElement).addEventListener('click',closePopup,false);
 
+  (heart as HTMLElement).addEventListener('click',()=>{
+    swoosh.play();
+    (inputEntry as HTMLElement).style.opacity = "0";
+    setTimeout(()=>{
+      (inputEntry as HTMLElement).style.display = 'none';
+    },200)
+  },false)
 }
 
-// function toggleSound() {
-//   const soundOn = document.getElementById('soundOn');
-//   const soundOff = document.getElementById('soundOff');
-//   if (!soundOn || !soundOff) return;
-//   if (soundReady == false) {
-//     soundOn.style.display = 'block';
-//     soundOff.style.display = 'none';
-//   } else {
-//     soundOn.style.display = 'none';
-//     soundOff.style.display = 'block';
-//   }
-//   soundReady=!soundReady;
-// }
+function setBalloonColor(color: string){
+  if(color == "0")
+    balloonMaterial.color.set(new THREE.Color(0xe91e1e))
+  else if(color == "1")
+    balloonMaterial.color.set(new THREE.Color(0xafeeee))
+  else if(color == "2")
+    balloonMaterial.color.set(new THREE.Color(0x90ee90))
+  else if(color == "3")
+    balloonMaterial.color.set(new THREE.Color(0xf0e68c))
+  else if(color == "4")
+    balloonMaterial.color.set(new THREE.Color(0xffc0cb))
+  else if(color == "5")
+    balloonMaterial.color.set(new THREE.Color(0xffffff))
+}
 
-// function toggleAnimation() {
-// }
+function toggleSound() {
+  swoosh.play();
+  const soundOn = document.getElementById('soundOn');
+  const soundOff = document.getElementById('soundOff');
+  if (!soundOn || !soundOff) return;
+  if (soundReady == false) {
+    soundOn.style.display = 'block';
+    soundOff.style.display = 'none';
+  } else {
+    soundOn.style.display = 'none';
+    soundOff.style.display = 'block';
+  }
+  soundReady=!soundReady;
+}
+// Function to show the popup element
+function showPopup(url: string) {
+  // Get the popup element
+  var popup = document.getElementById("popup");
+  
+  // Set the URL input value to the current URL with the user input appended as query parameters
+  var urlInput = document.getElementById("url-input");
+  (urlInput as HTMLInputElement).value = url;
+
+  var sendGift = document.getElementById("sendGift");
+  (sendGift as HTMLElement).innerText = 'Send your gift to '+((name2Input as HTMLInputElement).value || "You")
+
+  // Create the Twitter button
+  const twitterButton = document.getElementById("twitter-btn");
+  const text = 'Check out this cool Valentine\'s Day gift!'
+  var twitterUrl = 'https://twitter.com/intent/tweet?url=' + encodeURIComponent(url) + '&text=' + encodeURIComponent(text);
+  (twitterButton as HTMLAnchorElement).href = twitterUrl;
+  
+  // Create the WhatsApp button
+  const whatsappButton = document.getElementById("whatsapp-btn");
+  (whatsappButton as HTMLAnchorElement).href = `https://api.whatsapp.com/send?text=Check out my little gift for you! ❤️ ${encodeURIComponent(url)}`;
+
+  // Show the popup
+  (popup as HTMLElement).style.display = "flex";
+}
+
+// Function to close the popup element
+function closePopup() {
+  // Get the popup element
+  var popup = document.getElementById("popup");
+  // Hide the popup
+  (popup as HTMLElement).style.display = "none";
+}
+
+// Function to copy the URL to the clipboard
+function copyUrl() {
+  // Get the URL input element
+  var urlInput = document.getElementById("url-input");
+  
+  // Select the text in the URL input element
+  const text = (urlInput as HTMLInputElement).value;
+  
+  // Copy the selected text to the clipboard
+  navigator.clipboard.writeText(text).then(function() {
+    console.log('Copied to clipboard: ' + text);
+  }, function() {
+    console.error('Failed to copy to clipboard: ' + text);
+  });
+}
+
